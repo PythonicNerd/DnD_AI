d6ec22b471628edd9dd7431ae5e271db90c37156
PythonicNerd <neilaylor@hotmail.com>
2018-01-08 19:00:14 -0600

    Kinda got the Decision Tree working

--- app/controllers/creature_levels_controller.rb
+++ app/controllers/creature_levels_controller.rb
@@ -21,37 +21,39 @@ class CreatureLevelsController < ApplicationController
     formatted_data.push("1")
 
 
+    #start training classifier
+
     training_file = File.read("app\\assets\\json\\monsters.json") # Load JSON file full of Monster Data
 
 
-    training_data = []
     attributes = ['HP','AC','STR','DEX','CON',"INT","WIS","CHA"]
+    training_data = []
 
 
     JSON.parse(training_file).each do |f|
+      hp = f["hp"][0..2]
+      ac = f["hp"][0..1]
 
-      training_data.push( [ f["hp"], f["ac"], f["str"], f["dex"], f["con"], f["int"], f["wis"], f["cha"], f["cr"] ] )
+      training_data.push( [ hp, ac, f["str"], f["dex"], f["con"], f["int"], f["wis"], f["cha"] ] )
 
-      end
-
-      puts training_data.length
+    end
 
 
-      tree = DecisionTree::ID3Tree.new(attributes, training_data, "1", :discrete)
-      tree.train
+
+      File.write("app\\assets\\json\\test1.txt", training_data)
+
+      tree = DecisionTree::ID3Tree.new(['HP','AC','STR','DEX','CON',"INT","WIS","CHA"], training_data, "Elephant", :discrete)
+      dec_tree = DecisionTree::ID3Tree.new(attributes, training_data, "Elephant", :discrete)
+      dec_tree.train
 
 
   #  classifier = train()
 
     #decision = tree.predict(formatted_data)
-    decision = tree.predict(["12","22","213","12","34","53","125","32","1"])
+    test = ["59", "12", "18", "15", "16", "2", "13", "8"]
 
-    puts decision.class
-
-    puts "8888888888888888888888"
-    puts decision
-    puts "8888888888888888888888"
-
+  decision = dec_tree.predict(test)
+  puts "Predicted: #{decision} ... True decision: #{test.last}";
 
   end
 
